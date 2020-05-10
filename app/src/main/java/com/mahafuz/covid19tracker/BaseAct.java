b0404fe3490e5f219d62d85157fdd585ceab1d1c
Zaman3027 <mahafuzzaman02@gmail.com>
2020-05-10 22:40:13 +0530

    added fragment

--- app/src/main/java/com/mahafuz/covid19tracker/BaseAct.java
+++ app/src/main/java/com/mahafuz/covid19tracker/BaseAct.java
@@ -1,19 +1,29 @@
 package com.mahafuz.covid19tracker;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class BaseAct extends AppCompatActivity {
     ProgressDialog progressDialog;
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         progressDialog = new ProgressDialog(this);
-
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(
+                new ColorDrawable(Color.argb(0, 255, 255, 255)));
+        actionBar.setElevation(0);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(LayoutInflater.from(getApplicationContext()).inflate(R.layout.custom_toolbar, null));
     }
 
     void showDialog(String title, String message) {
--- app/src/main/java/com/mahafuz/covid19tracker/Home.java
+++ app/src/main/java/com/mahafuz/covid19tracker/Home.java
@@ -1,14 +1,17 @@
 package com.mahafuz.covid19tracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Home extends BaseAct {
+import com.mahafuz.covid19tracker.Fragment.HomeFragment;
 
+public class Home extends BaseAct {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.navigation_drawer_frame, new HomeFragment())
+                .commit();
+
     }
 }
