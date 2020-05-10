1ffef558a452d54ead1844e79cd45f29a49e85bf
Zaman3027 <mahafuzzaman02@gmail.com>
2020-05-10 23:39:06 +0530

    add fragment and view

--- app/src/main/java/com/mahafuz/covid19tracker/Fragment/HomeFragment.java
+++ app/src/main/java/com/mahafuz/covid19tracker/Fragment/HomeFragment.java
@@ -1,12 +1,20 @@
 package com.mahafuz.covid19tracker.Fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.mahafuz.covid19tracker.R;
 
@@ -16,11 +24,22 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    CardView homeCardActive, homeCardRecovered, homeDeadActive;
+    int screenWidth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        homeCardActive = getView().findViewById(R.id.homeCardActive);
+        homeCardRecovered = getView().findViewById(R.id.homeCardRecovered);
+        homeDeadActive = getView().findViewById(R.id.homeDeadActive);
+    }
+
 }
